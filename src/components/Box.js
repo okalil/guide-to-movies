844f77edb2cb01da546b2e3a6d3521267c3623a2
Andrew Kalil Barrense Magalhães <kalilmagal@gmail.com>
2021-09-13 16:53:33 -0300

    add box component

---
+++ src/components/Box.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Box = styled.section`
+  background: #fff;
+  border-radius: 7px;
+  border: 1px solid #ddd;
+`;
+
+export default Box;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Box from '../components/Box';
 
 export default function Home() {
   return (
@@ -10,25 +11,27 @@ export default function Home() {
         <input placeholder="Procurar filme..." />
       </div>
       <aside>
-        <span>Resultados da Busca</span>
-        <ul>
-          <li>
-            <span>Filmes</span>
-            <span>48</span>
-          </li>
-        </ul>
+        <Box>
+          <span>Resultados da Busca</span>
+          <ul>
+            <li>
+              <span>Filmes</span>
+              <span>48</span>
+            </li>
+          </ul>
+        </Box>
         <p>
           Dica: Você pode usar o filtro 'y:' para limitar seus resultados por
           ano. Exemplo: 'tropa de elite y:2007'
         </p>
       </aside>
       <main>
-        <section>
+        <Box>
           <img src="" alt="" />
           <h4>Os Vingadores</h4>
           <span>25 de abril de 2012</span>
           <p>Loki, o irmão de Thor ....</p>
-        </section>
+        </Box>
       </main>
     </div>
   );
